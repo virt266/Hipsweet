5556e582ea5eccc9bff2a056153ff2cedc16973f
Daynichenko Sergey <virt266@gmail.com>
2017-07-25 12:57:19 +0300

    tabs works

--- app/js/js.js
+++ app/js/js.js
@@ -1,9 +1,3 @@
- // $('#fullpage').fullpage({
- // 	anchors:['sections1', 'sections2','sections3','sections4','sections5','sections6', 'sections7'],
- // 	menu: '#myMenu',
- // 	slidesNavigation: true,
- // 	slidesNavPosition: 'bottom',
- // });
 $(document).ready(function(){
   $('.bxslider').bxSlider();
 });
