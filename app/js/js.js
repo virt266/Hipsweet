d7278f7529c1b11676822ff857ac9ade89c5771e
Daynichenko Sergey <virt266@gmail.com>
2017-07-26 00:10:28 +0300

    tabs works

--- app/js/js.js
+++ app/js/js.js
@@ -1,5 +1,28 @@
 $(document).ready(function(){
-  $('.bxslider').bxSlider();
+	$('.bxslider').bxSlider();
+	$('.tabs-link').on('click', function(e){
+		e.preventDefault();
+
+		var
+			$this = $(this),
+			item = $this.closest('.tabs-item'),
+			container = $this.closest('.wrapper'),
+			content = container.find('.tabs-content-item'),
+			ndx = item.index(),
+			reqItem = content.eq(ndx),
+			activeItem = content.filter('.tabs-item-active');
+
+			item.addClass('tabs-item-active')
+				.siblings()
+				.removeClass('tabs-item-active');
+
+			content.eq(ndx)
+				.addClass('content-active')
+				.siblings()
+				.removeClass('content-active');
+
+
+	});
 });
 
 jQuery(function($){
